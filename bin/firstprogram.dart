f5ae60471b1c6d78451a7207acb75a68da9cfd4e
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-03-14 05:58:20 +0530

    Some Changes

--- bin/firstprogram.dart
+++ bin/firstprogram.dart
@@ -1,9 +1,3 @@
 void main() {
-  // This is a single line comment
-  /*
-  This is a multiline comment
-  */
-  // Commnets are those read
-  /// by the compiler but not executed
   print("Hello New Programmer Learning Dipisha");
 }
